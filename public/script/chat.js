385ae84b39fcc37b5f8ec4d132f999ca9e37bea5
CAH9I <unlimmitedd@gmail.com>
2015-10-04 15:25:43 +0300

    static has added

---
+++ public/script/chat.js
@@ -0,0 +1,14 @@
+var Chat = function(socket) {
+  this.socket = socket;
+}
+
+Chat.prototype.sendMessage = function(text) {
+  var message = {
+    text: text
+  };
+  this.socket.emit('message', message);
+}
+
+Chat.prototype.changeName = function(name) {
+  this.socket.emit('nameAttempt', name);
+}
---
+++ public/script/chat_ui.js
@@ -0,0 +1,86 @@
+angular.module('chatApp', [])
+.directive('ownName', function() {
+  return {
+    template: '({{name}})'
+  };
+})
+.directive('nickName', function() {
+  return {
+    template: '{{message.name}}: '
+  };
+})
+.directive('messageText', function() {
+  return {
+    restrict: 'E',
+    template: '{{message.text}}'
+  }
+})
+.directive('scrollBottom', function() {
+  return {
+    scope: {
+      collection: '=scrollBottom'
+    },
+    link: function(scope, element) {
+      scope.$watchCollection('collection', function() {
+        $(element).scrollTop($(element).prop('scrollHeight'));
+      })
+    }
+  };
+})
+.factory('app', function($rootScope) {
+  var socket = io.connect();
+  var chat = new Chat(socket);
+  return {
+    socket: socket,
+    chat: chat
+  };
+})
+.controller('ChatController', function($scope, app) {
+  $scope.name = '';
+  $scope.newName = '';
+  $scope.messages = [];
+  $scope.currentMessage = '';
+  $scope.error = '';
+  $scope.isFormOpen = false;
+  
+  $scope.toggleForm = function() {
+    $scope.isFormOpen = !$scope.isFormOpen;
+    $scope.error = '';
+    $scope.newName = '';
+  }
+  
+  $scope.send = function($event) {
+    $event && $event.preventDefault();
+    if ($scope.currentMessage.trim() === '') {
+      $scope.currentMessage = '';
+      return;
+    }
+    app.chat.sendMessage($scope.currentMessage);
+    $scope.messages.push({name: $scope.name, text: $scope.currentMessage});
+    $scope.currentMessage = '';
+  };
+  
+  $scope.changeName = function(name) {
+    app.chat.changeName(name);
+  }
+  
+  app.socket.on('message', function(message) {
+    $scope.$apply(function() {
+      $scope.messages.push(message);
+    });
+  });  
+  
+  app.socket.on('nameResult', function(message) {
+    if (message.success) {
+      $scope.$apply(function() {
+        $scope.name = message.name;
+        $scope.newName = '';
+        $scope.error = '';
+      });
+    } else {
+      $scope.$apply(function() {
+        $scope.error = message.cause;
+      });
+    }
+  });
+});
